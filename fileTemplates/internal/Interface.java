a358ed6015a8b6d25d64f8b6b227771478932d8d
Jim <jim@jimdembp.lan>
2018-04-21 16:36:54 +0800

    IU-2017.2.6 <jim@jimdembp.lan Create markdown.xml, ide.general.xml, project.default.xml, diff.xml, Enum.java, Interface.java, git.xml, other.xml, Default.xml, code.style.schemes, find.xml, ui.lnf.xml, projectView.xml, colors.scheme.xml, path.macros.xml, databaseDrivers.xml, mavenVersion.xml, AnnotationType.java, Default.xml, Class.java, jdk.table.xml, debugger.renderers.xml, laf.xml, editor.codeinsight.xml, databaseSettings.xml, toStringTemplates.xml, keymap.xml, editor.xml, debugger.xml

---
+++ fileTemplates/internal/Interface.java
@@ -0,0 +1,10 @@
+#if (${PACKAGE_NAME} && ${PACKAGE_NAME} != "")package ${PACKAGE_NAME};#end
+#parse("File Header.java")
+/**
+ * This is Description
+ *
+ * @author ${USER}
+ * @date ${YEAR}/${MONTH}/${DAY}
+ */
+public interface ${NAME} {
+}
