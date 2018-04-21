8fae60ba2c24b08d71493697d4887c6edb735ccb
Jim <jim@JimdeMBP.lan>
2018-04-21 17:12:52 +0800

    增加项目

---
+++ README.md
@@ -0,0 +1,2 @@
+# maven-multi-module-demo
+Maven多模块项目样例以及构建教程
---
+++ multi-module-parent/multi-module-common/src/main/java/com/jsoft/demo/common/Tools.java
@@ -0,0 +1,19 @@
+package com.jsoft.demo.common;
+
+/**
+ * 工具类
+ *
+ * @author jim
+ * @date 2018/04/21
+ */
+public class Tools {
+    
+    /**
+     * 标准输出
+     * 
+     * @param msg 输出的信息
+     */
+    public static void print(String msg) {
+        System.out.println(msg);
+    }
+}
