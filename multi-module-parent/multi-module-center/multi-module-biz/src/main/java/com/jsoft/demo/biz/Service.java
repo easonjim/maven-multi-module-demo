e970a420ab564b225f6622498796e7f1de71dcf0
Jim <easonjim@163.com>
2018-04-23 13:04:13 +0800

    增加示例代码

---
+++ multi-module-parent/multi-module-center/multi-module-biz/src/main/java/com/jsoft/demo/biz/Service.java
@@ -0,0 +1,19 @@
+package com.jsoft.demo.biz;
+
+import com.jsoft.demo.common.Tools;
+
+/**
+ * Service
+ *
+ * @author jim
+ * @date 2018/04/23
+ */
+public class Service {
+
+    /**
+     * 业务层输出
+     */
+    public void out(){
+        Tools.println("biz->out");
+    }
+}
---
+++ multi-module-parent/multi-module-center/multi-module-webapp/src/main/java/com/jsoft/demo/webapp/Main.java
@@ -0,0 +1,19 @@
+package com.jsoft.demo.webapp;
+
+import com.jsoft.demo.biz.Service;
+import com.jsoft.demo.common.Tools;
+
+/**
+ * Main
+ *
+ * @author jim
+ * @date 2018/04/23
+ */
+public class Main {
+
+    public static void main(String[] args) {
+        Service service = new Service();
+        Tools.println("webapp->main");
+        service.out();
+    }
+}
